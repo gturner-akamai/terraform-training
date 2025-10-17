2686ceaaa2b2dd884bf3189feaaa3baba9f651a8
Garrick Turner <gturner@akamai.com>
2025-10-17 17:18:27 -0400

    Session 4 HW w/ for_each exercise

--- dns.tf
+++ dns.tf
@@ -7,3 +7,13 @@ resource "akamai_dns_record" "my_dns_hostnames" {
   name       = local.app_hostnames[count.index]
   target     = ["192.168.30.1"]
 }
+
+resource "akamai_dns_record" "dns_foreach_records" {
+  for_each = var.dns_records
+ 
+  zone       = each.value.zone
+  recordtype = each.value.recordType
+  ttl        = each.value.ttl
+  target     = [each.value.target]
+  name       = each.value.name
+}
--- variables.tf
+++ variables.tf
@@ -21,3 +21,30 @@ variable "edgerc_path" {
   description = "Full path to edgerc file"
   default     = "~/.edgerc"
 }
+
+
+variable "dns_records" {
+  default = {
+    "origin1" = {
+      zone       = "gturner.test"
+      recordType = "A"
+      ttl        = 60
+      target     = "172.233.190.92"
+      name       = "origin-www.example.com"
+    },
+    "origin2" = {
+      zone       = "gturner.test"
+      recordType = "A"
+      ttl        = 100
+      target     = "173.233.190.93"
+      name       = "origin-api.example.com"
+    },
+    "origin3" = {
+      zone       = "gturner.test"
+      recordType = "A"
+      ttl        = 600
+      target     = "174.233.190.94"
+      name       = "origin-blog.example.com"
+    }
+  }
+}
